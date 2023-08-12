350852f7258dbbe24541b3351f8fc1ae0b2656ea
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-08-12 00:43:19 -0300

    add auth page

---
+++ lib/src/feactures/auth/presenter/ui/pages/choose_auth_page.dart
@@ -0,0 +1,49 @@
+import 'package:app_receitas/src/core/widgets/cookie_button.dart';
+import 'package:app_receitas/src/core/widgets/cookie_text.dart';
+import 'package:app_receitas/src/feactures/auth/presenter/ui/organisms/custom_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class ChooseAuthPage extends StatelessWidget {
+  const ChooseAuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScreen(
+      iconAppBar: IconButton(
+        onPressed: () {},
+        icon: const Icon(Icons.sunny),
+      ),
+      image:
+          'https://media.discordapp.net/attachments/1071892919633576117/1139340986322014208/image.png?width=518&height=518',
+      backgroundImage: '',
+      body: Column(
+        children: [
+          CookieText(
+            text: AppLocalizations.of(context)!.chooseAuthTitle,
+            typography: CookieTypography.title,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 10),
+          CookieText(
+            text: AppLocalizations.of(context)!.chooseAuthBody,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+      bottomBar: Column(
+        children: [
+          CookieButton.outline(
+            label: AppLocalizations.of(context)!.chooseAuthRegisterButton,
+            onPressed: () => Navigator.pushNamed(context, '/register'),
+          ),
+          const SizedBox(height: 10),
+          CookieButton(
+            label: AppLocalizations.of(context)!.chooseAuthLoginButton,
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
