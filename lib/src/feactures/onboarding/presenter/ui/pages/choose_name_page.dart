bcc4460068cf211f7e83a165201c7c35690f6857
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-10-23 13:47:11 -0300

    Feat verification name

--- lib/src/feactures/onboarding/presenter/ui/pages/choose_name_page.dart
+++ lib/src/feactures/onboarding/presenter/ui/pages/choose_name_page.dart
@@ -6,10 +6,16 @@ import 'package:app_receitas/src/feactures/onboarding/presenter/controller/onboa
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class ChooseNamePage extends StatelessWidget {
+class ChooseNamePage extends StatefulWidget {
   final OnBoardingController ct;
   const ChooseNamePage({super.key, required this.ct});
 
+  @override
+  State<ChooseNamePage> createState() => _ChooseNamePageState();
+}
+
+class _ChooseNamePageState extends State<ChooseNamePage> {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +37,7 @@ class ChooseNamePage extends StatelessWidget {
           children: [
             IconButton(
               onPressed: () {
-                ct.pageController.animateToPage(
+                widget.ct.pageController.animateToPage(
                   2,
                   duration: const Duration(milliseconds: 400),
                   curve: Curves.ease,
@@ -40,6 +46,8 @@ class ChooseNamePage extends StatelessWidget {
               icon: const Icon(Icons.arrow_back_ios_new),
             ),
             Form(
+              key: formKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
@@ -56,13 +64,21 @@ class ChooseNamePage extends StatelessWidget {
                   const SizedBox(height: 10),
                   CookieTextField(
                     hintText: AppLocalizations.of(context)!.chooseNameHintText,
+                    controller: widget.ct.userNameController,
+                    validator: (value) {
+                      if (value != null && value.length < 3) {
+                        return 'O nome deve ter no minimo 3 letras';
+                      }
+                      return null;
+                    },
                   ),
                   const SizedBox(height: 20),
                   CookieButton(
-                    label: AppLocalizations.of(context)!.difficultyRecipesConfirm,
+                    label:
+                        AppLocalizations.of(context)!.difficultyRecipesConfirm,
                     onPressed: () {
-                      ct.saveIsLogin();
-                      ct.creatFoodPref();
+                      widget.ct.saveIsLogin();
+                      widget.ct.creatFoodPref();
                       Navigator.pushNamedAndRemoveUntil(
                         context,
                         HomePage.route,
