35fc11e7cf6a460fc2b6f7c98cdd11d97b8a202a
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-12-26 14:33:03 -0300

    feat add container in recipe view

--- lib/src/feactures/recipes/presenter/ui/moleculs/view_details_recipe.dart
+++ lib/src/feactures/recipes/presenter/ui/moleculs/view_details_recipe.dart
@@ -1,25 +1,26 @@
 import 'package:app_receitas/src/core/widgets/cookie_text.dart';
 import 'package:app_receitas/src/feactures/recipes/presenter/ui/atomic/custom_container.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class ViewDetailsRecipe extends StatelessWidget {
   const ViewDetailsRecipe({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        CookieText(
+        const CookieText(
           text: 'Detalhes da receita',
           typography: CookieTypography.title,
         ),
-        SizedBox(height: 20),
-        CookieText(
+        const SizedBox(height: 20),
+        const CookieText(
           text:
               'Lorem ipsum dolor sit amet consectetur. Sodales donec non tortor augue eros massa feugiat. Odio vitae montes in pharetra ac. Pulvinar dignissim accumsan bibendum enim id enim eu vitae viverra. In massa erat tristique mauris consequat habitant aenean orci lacus.',
         ),
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
         CustomContainer(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -27,37 +28,36 @@ class ViewDetailsRecipe extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  CookieText(
-                      text: 'Ingredientes', typography: CookieTypography.title),
-                  Icon(Icons.accessible_forward, size: 30),
+                  const CookieText(
+                    text: 'Ingredientes',
+                    typography: CookieTypography.title,
+                  ),
+                  SvgPicture.asset('assets/icons/carrot.svg'),
                 ],
               ),
-              SizedBox(height: 10),
-              CookieText(text: '● banana 300ml'),
-              CookieText(text: '● banana 300ml'),
-              CookieText(text: '● banana 300ml'),
+              const SizedBox(height: 10),
+              const CookieText(text: '● banana 300ml'),
+              const CookieText(text: '● banana 300ml'),
+              const CookieText(text: '● banana 300ml'),
             ],
           ),
         ),
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
         CustomContainer(
           child: Column(
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  CookieText(
+                  const CookieText(
                     text: 'Instruções de preparo',
                     typography: CookieTypography.title,
                   ),
-                  Icon(
-                    Icons.access_alarm_outlined,
-                    size: 30,
-                  )
+                  SvgPicture.asset('assets/icons/knife.svg'),
                 ],
               ),
-              SizedBox(height: 20),
-              Row(
+              const SizedBox(height: 20),
+              const Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   CookieText(
@@ -73,6 +73,42 @@ class ViewDetailsRecipe extends StatelessWidget {
                 ],
               ),
             ],
+          ),
+        ),
+        const SizedBox(height: 20),
+        Visibility(
+          visible: true,
+          child: CustomContainer(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const CookieText(
+                      text: 'Hora de se servir!',
+                      typography: CookieTypography.title,
+                    ),
+                    SvgPicture.asset('assets/icons/pan.svg'),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                const Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CookieText(
+                      text: '1.',
+                    ),
+                    SizedBox(width: 5),
+                    Expanded(
+                      child: CookieText(
+                        text:
+                            'Enxágue o arroz basmati até que a água fique clara.',
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ],
