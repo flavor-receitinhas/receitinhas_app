103a5bdf3796937be2eb3f9465bee5d4c1a0ff9c
lucas <lucas.cardo123@hotmail.com>
2024-02-16 11:22:34 -0400

    Update global_variables.dart

--- lib/src/core/global/global_variables.dart
+++ lib/src/core/global/global_variables.dart
@@ -1,4 +1,7 @@
 import 'package:get_it/get_it.dart';
 
 GetIt di = GetIt.instance;
-String apiUrl = 'http://192.168.1.9:8080';
+
+class Global {
+  static const String dnsApi = 'http://192.168.1.9:8080';
+}
