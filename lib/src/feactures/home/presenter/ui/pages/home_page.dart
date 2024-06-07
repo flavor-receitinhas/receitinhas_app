4cafed82081c46f988c511d46588f47052d25c9f
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2024-06-07 11:23:13 -0300

    fix home state

--- lib/src/feactures/home/presenter/ui/pages/home_page.dart
+++ lib/src/feactures/home/presenter/ui/pages/home_page.dart
@@ -21,6 +21,15 @@ class _HomePageState extends State<HomePage> {
   final HomeController ct = di();
 
   @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) => ct.init(context));
+    ct.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return CookiePage(
       state: ct.state,
