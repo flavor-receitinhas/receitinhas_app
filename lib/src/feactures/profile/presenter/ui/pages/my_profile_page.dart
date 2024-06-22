04e085623d1568a5b2080999bba18b66af56a233
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2024-06-22 14:26:31 -0300

    refactor variable image

--- lib/src/feactures/profile/presenter/ui/pages/my_profile_page.dart
+++ lib/src/feactures/profile/presenter/ui/pages/my_profile_page.dart
@@ -140,7 +140,7 @@ class _MyProfilePageState extends State<MyProfilePage> {
                                   ClipRRect(
                                     borderRadius: BorderRadius.circular(10),
                                     child: Image.network(
-                                      'https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                                      Global.imageRecipeDefault,
                                     ),
                                   ),
                                   Padding(
