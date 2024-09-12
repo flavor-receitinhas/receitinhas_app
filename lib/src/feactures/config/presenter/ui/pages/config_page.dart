4294e072fbf954745aa70519bba4fe30d5a3ce89
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2024-09-12 19:51:33 -0300

    refactor version in config

--- lib/src/feactures/config/presenter/ui/pages/config_page.dart
+++ lib/src/feactures/config/presenter/ui/pages/config_page.dart
@@ -1,6 +1,4 @@
-import 'package:app_receitas/src/core/global/global_variables.dart';
 import 'package:app_receitas/src/core/widgets/cookie_export.dart';
-import 'package:app_receitas/src/core/widgets/feactures/cookie_text.dart';
 import 'package:app_receitas/src/feactures/config/presenter/controllers/config_controller.dart';
 import 'package:app_receitas/src/feactures/config/presenter/ui/atomic/introduce_config.dart';
 import 'package:app_receitas/src/feactures/config/presenter/ui/moleculs/container_config.dart';
@@ -11,6 +9,7 @@ import 'package:app_receitas/src/feactures/config/presenter/ui/pages/edit_accoun
 import 'package:app_receitas/src/feactures/config/presenter/ui/pages/privacy_policy_page.dart';
 import 'package:app_receitas/src/feactures/config/presenter/ui/pages/term_page.dart';
 import 'package:flutter/material.dart';
+import 'package:page_manager/manager_page.dart';
 
 class ConfigPage extends StatefulWidget {
   static const route = 'config';
@@ -20,153 +19,146 @@ class ConfigPage extends StatefulWidget {
   State<ConfigPage> createState() => _ConfigPageState();
 }
 
-class _ConfigPageState extends State<ConfigPage> {
-  final ConfigController ct = di();
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
+class _ConfigPageState extends ManagerPage<ConfigController, ConfigPage> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context).colorScheme;
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-          child: ListView(
-            children: [
-              const IntroduceConfig(),
-              const SizedBox(height: 20),
-              CookieText(
-                text: 'Conta',
-                typography: CookieTypography.button,
-                color: theme.onPrimary,
-              ),
-              const SizedBox(height: 10),
-              ContainerConfig(
-                icon: 'assets/icons/person.svg',
-                text: 'Editar Conta',
-                action: const Icon(Icons.chevron_right),
-                onTap: () {
-                  Navigator.pushNamed(context, EditAccountPage.route);
-                },
-              ),
-              const SizedBox(height: 10),
-              ContainerConfig(
-                icon: 'assets/icons/key.svg',
-                text: 'Alterar senha',
-                action: const Icon(Icons.chevron_right),
-                onTap: () {
-                  Navigator.pushNamed(
-                    context,
-                    ChangedPasswordPage.route,
-                  );
-                },
-              ),
-              const SizedBox(height: 20),
-              CookieText(
-                text: 'Personalização',
-                typography: CookieTypography.button,
-                color: theme.onPrimary,
-              ),
-              const SizedBox(height: 10),
-              ContainerConfig(
-                icon: 'assets/icons/moon.svg',
-                text: 'Modo notuno',
-                action: SizedBox(
-                  height: 20,
-                  child: Transform.scale(
-                    scale: 0.8,
-                    child: Switch(
-                      thumbIcon: WidgetStateProperty.all(
-                        ct.darkTheme
-                            ? const Icon(
-                                Icons.light_mode,
-                                color: Colors.white,
-                              )
-                            : const Icon(
-                                Icons.dark_mode,
-                                color: Colors.white,
-                              ),
-                      ),
-                      activeColor: theme.primary,
-                      value: ct.darkTheme,
-                      onChanged: (value) {
-                        ct.changeTheme();
-                      },
+    return CookiePage(
+      state: ct.state,
+      done: () => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        child: ListView(
+          children: [
+            const IntroduceConfig(),
+            const SizedBox(height: 20),
+            CookieText(
+              text: 'Conta',
+              typography: CookieTypography.button,
+              color: theme.onPrimary,
+            ),
+            const SizedBox(height: 10),
+            ContainerConfig(
+              icon: 'assets/icons/person.svg',
+              text: 'Editar Conta',
+              action: const Icon(Icons.chevron_right),
+              onTap: () {
+                Navigator.pushNamed(context, EditAccountPage.route);
+              },
+            ),
+            const SizedBox(height: 10),
+            ContainerConfig(
+              icon: 'assets/icons/key.svg',
+              text: 'Alterar senha',
+              action: const Icon(Icons.chevron_right),
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  ChangedPasswordPage.route,
+                );
+              },
+            ),
+            const SizedBox(height: 20),
+            CookieText(
+              text: 'Personalização',
+              typography: CookieTypography.button,
+              color: theme.onPrimary,
+            ),
+            const SizedBox(height: 10),
+            ContainerConfig(
+              icon: 'assets/icons/moon.svg',
+              text: 'Modo notuno',
+              action: SizedBox(
+                height: 20,
+                child: Transform.scale(
+                  scale: 0.8,
+                  child: Switch(
+                    thumbIcon: WidgetStateProperty.all(
+                      ct.darkTheme
+                          ? const Icon(
+                              Icons.light_mode,
+                              color: Colors.white,
+                            )
+                          : const Icon(
+                              Icons.dark_mode,
+                              color: Colors.white,
+                            ),
                     ),
+                    activeColor: theme.primary,
+                    value: ct.darkTheme,
+                    onChanged: (value) {
+                      ct.changeTheme();
+                    },
                   ),
                 ),
               ),
-              const SizedBox(height: 20),
-              CookieText(
-                text: 'Personalização',
-                typography: CookieTypography.button,
-                color: theme.onPrimary,
+            ),
+            const SizedBox(height: 20),
+            CookieText(
+              text: 'Personalização',
+              typography: CookieTypography.button,
+              color: theme.onPrimary,
+            ),
+            const SizedBox(height: 10),
+            ContainerConfig(
+              icon: 'assets/icons/eye.svg',
+              text: 'Politicas de privacidade',
+              action: const Icon(Icons.chevron_right),
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  PrivacyPolicyPage.route,
+                );
+              },
+            ),
+            const SizedBox(height: 10),
+            ContainerConfig(
+              icon: 'assets/icons/document.svg',
+              text: 'Termos e condições',
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  TermPage.route,
+                );
+              },
+              action: const Icon(Icons.chevron_right),
+            ),
+            const SizedBox(height: 10),
+            ContainerConfig(
+              icon: 'assets/icons/persons.svg',
+              text: 'Nossa equipe',
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  AboutTeamPage.route,
+                );
+              },
+              action: const Icon(Icons.chevron_right),
+            ),
+            const SizedBox(height: 10),
+            ContainerConfig(
+              icon: 'assets/icons/sing_out.svg',
+              text: 'Sair da conta',
+              textColor: const Color(0xffFF5757),
+              action: const Icon(Icons.chevron_right),
+              onTap: () {
+                LeaveAlertDialog(ct: ct).show(context);
+              },
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: CookieText(
+                text: 'Versão do Aplicativo',
+                color: theme.onPrimary.withOpacity(0.5),
               ),
-              const SizedBox(height: 10),
-              ContainerConfig(
-                icon: 'assets/icons/eye.svg',
-                text: 'Politicas de privacidade',
-                action: const Icon(Icons.chevron_right),
-                onTap: () {
-                  Navigator.pushNamed(
-                    context,
-                    PrivacyPolicyPage.route,
-                  );
-                },
+            ),
+            Center(
+              child: CookieText(
+                text:
+                    '${ct.packageInfo?.version} (${ct.packageInfo?.buildNumber})',
+                color: theme.onPrimary.withOpacity(0.5),
               ),
-              const SizedBox(height: 10),
-              ContainerConfig(
-                icon: 'assets/icons/document.svg',
-                text: 'Termos e condições',
-                onTap: () {
-                  Navigator.pushNamed(
-                    context,
-                    TermPage.route,
-                  );
-                },
-                action: const Icon(Icons.chevron_right),
-              ),
-              const SizedBox(height: 10),
-              ContainerConfig(
-                icon: 'assets/icons/persons.svg',
-                text: 'Nossa equipe',
-                onTap: () {
-                  Navigator.pushNamed(
-                    context,
-                    AboutTeamPage.route,
-                  );
-                },
-                action: const Icon(Icons.chevron_right),
-              ),
-              const SizedBox(height: 10),
-              ContainerConfig(
-                icon: 'assets/icons/sing_out.svg',
-                text: 'Sair da conta',
-                textColor: const Color(0xffFF5757),
-                action: const Icon(Icons.chevron_right),
-                onTap: () {
-                  LeaveAlertDialog(ct: ct).show(context);
-                },
-              ),
-              const SizedBox(height: 20),
-              Center(
-                child: CookieText(
-                  text: 'Versão do Aplicativo',
-                  color: theme.onPrimary.withOpacity(0.5),
-                ),
-              ),
-              Center(
-                child: CookieText(
-                  text: '0.0.1',
-                  color: theme.onPrimary.withOpacity(0.5),
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
