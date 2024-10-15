9406615dc72b1cd0d9f001fc236153acf48a238f
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2024-10-15 11:22:22 -0300

    feat PopScope in recipe

--- lib/src/feactures/recipes/presenter/ui/pages/create_recipe_page.dart
+++ lib/src/feactures/recipes/presenter/ui/pages/create_recipe_page.dart
@@ -1,4 +1,7 @@
+import 'package:app_receitas/src/core/widgets/cookie_export.dart';
+import 'package:app_receitas/src/feactures/home/presenter/ui/pages/custom_bottom_bar.dart';
 import 'package:app_receitas/src/feactures/recipes/presenter/controller/create_recipe_controller.dart';
+import 'package:app_receitas/src/feactures/recipes/presenter/ui/atomic/leave_recipe_sheet.dart';
 import 'package:app_receitas/src/feactures/recipes/presenter/ui/pages/create_view_recipe_page.dart';
 import 'package:app_receitas/src/feactures/recipes/presenter/ui/pages/info_create_page.dart';
 import 'package:app_receitas/src/feactures/recipes/presenter/ui/pages/ingredient_create_page.dart';
@@ -7,6 +10,7 @@ import 'package:app_receitas/src/feactures/recipes/presenter/ui/pages/intruction
 import 'package:app_receitas/src/feactures/recipes/presenter/ui/pages/server_dish_page.dart';
 import 'package:flutter/material.dart';
 import 'package:page_manager/manager_page.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class CreateRecipePage extends StatefulWidget {
   static const route = '/create-recipe';
@@ -35,11 +39,33 @@ class _CreateRecipePageState
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      onPageChanged: ct.onChangedPage,
-      controller: ct.pageController,
-      physics: const NeverScrollableScrollPhysics(),
-      children: routes,
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) {
+        if (didPop) {
+          return;
+        }
+        ct.showDialogDiscard()
+            ? CookieSheetBottom(
+                title: CookieText(
+                  text: AppLocalizations.of(context)!.recipeDiscardPrompt,
+                  color: Theme.of(context).colorScheme.onSecondary,
+                  typography: CookieTypography.title,
+                ),
+                body: const LeaveRecipeSheet(),
+              ).show(context)
+            : Navigator.pushNamedAndRemoveUntil(
+                context,
+                CustomBottomBar.route,
+                (route) => false,
+              );
+      },
+      child: PageView(
+        onPageChanged: ct.onChangedPage,
+        controller: ct.pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: routes,
+      ),
     );
   }
 }
